a17f889e151d8d74731bf620d097ced2f43421bb
Mega-PC <tlijanihachem4@gmail.com>
2024-03-04 17:56:58 +0100

    Cloud Functions Deployment Workflow

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -13,7 +13,7 @@ module.exports = {
                 "node": true
             },
             "files": [
-                ".eslintrc.{js,cjs}"
+                ".eslintrc.{js,cjs,ts}"
             ],
             "parserOptions": {
                 "sourceType": "script"
