04af6b53a2e69c7e3b9bd98c52b34ab4ddb80153
hachem tlijani <tlijanihachem4@gmail.com>
2024-03-05 23:29:01 +0100

    fixed lin and test  with ts

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -1,32 +1,33 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended"
-    ],
-    "overrides": [
-        {
-            "env": {
-                "node": true
-            },
-            "files": [
-                ".eslintrc.{js,cjs,.ts}"
-            ],
-            "parserOptions": {
-                "sourceType": "script"
-            },
-        }
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": "latest"
-    },
-    "plugins": [
-        "@typescript-eslint"
-    ],
-    "rules": {
-    },
-}
+  root: true,
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:import/errors",
+    "plugin:import/warnings",
+    "plugin:import/typescript",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: ["tsconfig.json", "tsconfig.dev.json"],
+    sourceType: "module",
+  },
+  ignorePatterns: [
+    "/lib/**/*", // Ignore built files.
+    "jest.config.js"
+
+  ],
+  plugins: [
+    "@typescript-eslint",
+    "import",
+  ],
+  rules: {
+    "quotes": ["error", "double"],
+    "import/no-unresolved": 0,
+    "max-len": 0,
+  },
+};
--- functions/index.ts
+++
@@ -1,4 +0,0 @@
-import * as functions from '@google-cloud/functions-framework';
-functions.http('helloGET', (req, res) => {
-  res.send('Hello World!');
-});
